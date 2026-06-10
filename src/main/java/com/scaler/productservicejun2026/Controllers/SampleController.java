7b7244125842e1af6e77890d649cdf13dcca27a8
Vishal Kumar <vk1607373@gmail.com>
2026-06-10 16:00:12 +0530

    implimentation of sample api having url variable

--- src/main/java/com/scaler/productservicejun2026/Controllers/SampleController.java
+++ src/main/java/com/scaler/productservicejun2026/Controllers/SampleController.java
@@ -1,25 +1,36 @@
 package com.scaler.productservicejun2026.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 /*
-http://localhost:8080/sample/hello
+http://localhost:8080/sample/hello/vishal
  */
 
 @RestController
 @RequestMapping("/sample")
 public class SampleController {
 
-    @GetMapping("hello")
-    public String sayHello(){
-        return "Hello Vishal Welcome";
+    @GetMapping("/hello/{name}/{numberOfTime}")
+    public String sayHello(@PathVariable String name , @PathVariable("numberOfTime") int number){
+        String response = "";
+        for(int i=1;i<=number;i++){
+            String s ="Hello "+name+"<br>";
+            response+=s;
+        }
+        return response;
     }
 
-    @GetMapping("/bye")
-    public String sayBye(){
-       return "Bye Vishal take care";
+    @GetMapping("/bye/{name}/{number}")
+    public String sayBye(@PathVariable("name") String input , @PathVariable int number){
+        String response ="";
+        for(int i=1;i<=number;i++){
+            String s = "bye "+input+"<br>";
+            response+=s;
+        }
+       return response;
     }
 }
